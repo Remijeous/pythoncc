7e870b69826274b1f37d5198f5e20d771b8006fa
Remi_Je_RJ <remijeous@gmail.com>
2023-06-01 23:57:45 +0000

    Deleted dupliacted files

--- dict_1.py
+++
@@ -1,4 +0,0 @@
-alien_0 = {'color' : 'green', 'points' : 5}
-
-new_points = alien_0['points']
-print("You have just earned " + str(new_points) + " points")
--- fav_languages.py
+++
@@ -1,9 +0,0 @@
-fav_languages = {
-    'rey' : 'python',
-    'jim' : 'c++',
-    'stuart' : 'Java',
-    'max' : 'kotlin'
-}
-
-for name, language in fav_languages.items():
-    print(name.title(), "favourite programming language is " + language.title() + "\n")
