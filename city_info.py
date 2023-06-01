09ab144ab97cb73a612a0d747ae6a0566c7cb43d
Remi_Je_RJ <remijeous@gmail.com>
2023-06-01 23:42:35 +0000

    Program updated based on PEP8 guidelines

--- city_info.py
+++ city_info.py
@@ -1,14 +1,24 @@
+"""this program is about print popular ciies in india and interesting this about them"""
 popular_cities = {
-    'mumbai':{'state':'maharastira', 'population':'19.98 million', 'fact':'financial capital of india'},
-    'delhi':{'state':'delhi territory','population':'16.31 million','fact':'capital city of india'},
-    'bangalore':{'state':'karnataka','population':'14.52 million', 'fact':'silicon valley on india'}
+    'mumbai':{
+        'state':'maharastira', 
+        'population':'19.98 million', 
+        'fact':'financial capital of india'},
+    'delhi':{
+        'state':'delhi territory',
+        'population':'16.31 million',
+        'fact':'capital city of india'},
+    'bangalore':{
+        'state':'karnataka',
+        'population':'14.52 million', 
+        'fact':'silicon valley on india'}
 }
 
 for city, city_info in popular_cities.items():
     print("\nCity Name is " + city.title())
-    state = city_info['state']
-    population = city_info['population']
-    fact = city_info['fact']
-    print("State Name:\t" + state.title())
-    print("Population:\t" + population.title())
-    print("Fact:\t\t" + fact.title())
+    STATE = city_info['state']
+    POPULATION = city_info['population']
+    FACT = city_info['fact']
+    print("State Name:\t" + STATE.title())
+    print("Population:\t" + POPULATION.title())
+    print("Fact:\t\t" + FACT.title())
