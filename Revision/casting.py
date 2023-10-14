026e0c4a83a69e71cc7d24e2157544376651a095
remijeous <remijeous@gmail.com>
2023-10-14 22:59:28 +0800

    14-10-23 11pm

--- Revision/casting.py
+++ Revision/casting.py
@@ -1,4 +1,4 @@
-a=int(input("Enter value for A: "))
-b=int(input("Enter value for B: "))
-c=a+b
-print(f'Total = {c})
+name = input('What is your Name: ')
+age = int(input("Enter Your Age: "))
+print('My Name is ',name)
+print('My age is ',age)
