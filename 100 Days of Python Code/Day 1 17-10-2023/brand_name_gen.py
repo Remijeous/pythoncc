b6a5b90f72632115934092c39a7ee57493406afc
remijeous <remijeous@gmail.com>
2023-10-17 23:14:15 +0800

    Day 1 17-10-23

---
+++ 100 Days of Python Code/Day 1 17-10-2023/brand_name_gen.py
@@ -0,0 +1,5 @@
+print("Welcome to Band Name Generator!!")
+city_name = input("Enter your city Name: ")
+pet_name = input("Enter your pet name: ")
+print("Your band name could be: " + pet_name + " " + city_name)
+
