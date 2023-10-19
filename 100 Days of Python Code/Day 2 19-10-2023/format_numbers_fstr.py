82623a78ba0ef74da570b0709d3ca762bb4e57d3
remijeous <remijeous@gmail.com>
2023-10-19 17:46:27 +0800

    19-10-23 6pm

---
+++ 100 Days of Python Code/Day 2 19-10-2023/format_numbers_fstr.py
@@ -0,0 +1,7 @@
+price = 120
+quantity = 2
+total = price * quantity
+
+print(f"Price: {price: .2f}") #format price with 2 decimal places
+print(f"Quantity: {quantity:d}") #format quantity as an integer
+print(f"total: {total: .2f}") #format total with 2 decimal places
