c143d93589dabed760364ce889d6caa627b39401
remijeous <remijeous@gmail.com>
2023-10-19 19:17:01 +0800

    19-10-23 7Pm

---
+++ 100 Days of Python Code/Day 2 19-10-2023/imp_math.py
@@ -0,0 +1,13 @@
+import math
+
+number = 123
+
+square = number ** 2
+cube = number ** 3
+squr_root = math.sqrt(number)
+cube_root = number ** (1/3)
+
+print(f"Square: {square}")
+print(f"Cube: {cube}")
+print(f"Square Root: {squr_root:.2f}")
+print(f"Cube Root: {cube_root:.2f}")
