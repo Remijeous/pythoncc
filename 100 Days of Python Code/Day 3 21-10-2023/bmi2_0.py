66a68993886e56a4e9590ae39dd2369856a62a46
remijeous <remijeous@gmail.com>
2023-10-21 18:55:43 +0800

    Day 3 21-10-2023 6.55pm

--- 100 Days of Python Code/Day 3 21-10-2023/bmi2_0.py
+++ 100 Days of Python Code/Day 3 21-10-2023/bmi2_0.py
@@ -1,9 +1,5 @@
-# Enter your height in meters e.g., 1.55
 height = float(input("Enter Height in meters: "))
-# Enter your weight in kilograms e.g., 72
 weight = int(input("Enter Weight: "))
-
-#Write your code below this line 👇
 bmi = weight / (height * height)
 if bmi < 18.5:
   print(f"Your BMI is {bmi}, you are underweight.")
