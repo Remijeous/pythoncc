a99005a9e8930be29ccca06734dae7b43b55f146
remijeous <remijeous@gmail.com>
2023-10-21 14:04:08 +0800

    Day-3 21-10-2023 2.04PM

--- 100 Days of Python Code/Day 3 21-10-2023/rollercoaster.py
+++ 100 Days of Python Code/Day 3 21-10-2023/rollercoaster.py
@@ -3,7 +3,9 @@ height = int(input("Enter your Height in cm: "))
 if height >= 120:
     print("Hi, You can ride the roolercoaster!!")
     age = int(input("Enter your age: "))
-    if age <= 18:
+    if age <=12:
+        print("Your Ticket Price is $5")
+    elif age < 18:
         print("Your Ticket Price is $7")
     else:
         print("Your Ticket Price is $12")
