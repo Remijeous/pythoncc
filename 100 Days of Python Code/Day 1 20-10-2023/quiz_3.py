030858c28823a5701eea49eb1bbbfb7bdb6a9a59
remijeous <remijeous@gmail.com>
2023-10-21 12:20:29 +0800

    Day 3 21-10-2023 12.20pm

---
+++ 100 Days of Python Code/Day 1 20-10-2023/quiz_3.py
@@ -0,0 +1,6 @@
+# num = "5"
+# print("Ordered"+num+"Pizzas")
+
+# print(6 + 4 / 2 - (1 * 2))
+a = int("5") / int(2.7)
+print(a)
---
+++ 100 Days of Python Code/Day 3 21-10-2023/rollercoaster.py
@@ -0,0 +1,6 @@
+print("Welcome to Ding Dong Rollercoaster")
+height = int(input("Enter your Height in cm: "))
+if height > 120:
+    print("Hi, You can ride the roolercoaster!!")
+else:
+    print("Sorry, you have to grow taller before you ride the coaster.")
