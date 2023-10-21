1657a837ea8a7f14df77df2f176a0905dbf81fce
remijeous <remijeous@gmail.com>
2023-10-21 13:45:27 +0800

    Day 3 21-10-23 1.45pm

--- 100 Days of Python Code/Day 3 21-10-2023/rollercoaster.py
+++ 100 Days of Python Code/Day 3 21-10-2023/rollercoaster.py
@@ -1,6 +1,11 @@
 print("Welcome to Ding Dong Rollercoaster")
 height = int(input("Enter your Height in cm: "))
-if height > 120:
+if height >= 120:
     print("Hi, You can ride the roolercoaster!!")
+    age = int(input("Enter your age: "))
+    if age <= 18:
+        print("Your Ticket Price is $7")
+    else:
+        print("Your Ticket Price is $12")
 else:
     print("Sorry, you have to grow taller before you ride the coaster.")
