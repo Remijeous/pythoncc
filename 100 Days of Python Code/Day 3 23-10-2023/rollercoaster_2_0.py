fdbf70008fc0208da2ea4e35489e921d43bc620a
remijeous <remijeous@gmail.com>
2023-10-24 07:41:47 +0800

    Day 3 24-20-2023 7.41am

--- 100 Days of Python Code/Day 3 23-10-2023/rollercoaster_2_0.py
+++ 100 Days of Python Code/Day 3 23-10-2023/rollercoaster_2_0.py
@@ -9,12 +9,15 @@ if height >= 120:
     elif age < 18:
         bill = 7
         print("Your Ticket Price is $7")
+    elif age >= 45 and age <= 55:
+        bill = 0
+        print("You can ride Free")
     else:
         bill = 12
-        print("Your Ticket Price is $12")
+        print("Your Ticket Price is $12")   
 
     photo_taken = input("You want to take photo? Type 'Y' for Yes and or'N' for No: ")
-    if photo_taken == "Y" or "y":
+    if photo_taken == "Y" and bill > 0:
         bill += 3
     print(f"Your final bill is ${bill}")
 else:
