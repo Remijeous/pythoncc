5c7d2abeffe00c93b86eee767a60468777bdcb7e
remijeous <remijeous@gmail.com>
2023-11-02 07:50:51 +0800

    Day 7 02-11-2023

---
+++ 100 Days of Python Code/Day 7 01-11-2023/hangman.py
@@ -0,0 +1,87 @@
+import random
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+
+end_of_game = False
+word_list = ["aardvark", "baboon", "camel"]
+chosen_word = random.choice(word_list)
+word_len = len(chosen_word)
+lives = 6
+print(f'Pssst, the solution is {chosen_word}.')
+
+display = []
+
+for _ in range(word_len):
+    display += "_"
+
+while not end_of_game:
+    guess = input("Guess a letter: ").lower()
+
+    for position in range(word_len):
+        letter = chosen_word[position]
+        if letter == guess:
+            display[position] = letter
+    
+    if guess not in chosen_word:
+        lives -= 1
+        if lives == 0:
+            end_of_game = True
+            print("You Lose.")
+
+    if "_" not in display:
+        end_of_game = True
+        print("You Win!")
