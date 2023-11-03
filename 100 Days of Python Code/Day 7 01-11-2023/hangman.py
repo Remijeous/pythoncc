455957f70851126945aa8cbe212397594914dd6d
remijeous <remijeous@gmail.com>
2023-11-04 07:51:54 +0800

    Day 7 04-10-2023 8am

--- 100 Days of Python Code/Day 7 01-11-2023/hangman.py
+++ 100 Days of Python Code/Day 7 01-11-2023/hangman.py
@@ -1,63 +1,9 @@
 import random
-stages = ['''
-  +---+
-  |   |
-  O   |
- /|\  |
- / \  |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
- /    |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|   |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
-  |   |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
-      |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-      |
-      |
-      |
-      |
-=========
-''']
+from hangman_words import word_list
+from hangman_art import stages, logo
 
 end_of_game = False
-word_list = ["aardvark", "baboon", "camel"]
+
 chosen_word = random.choice(word_list)
 word_len = len(chosen_word)
 lives = 6
@@ -71,17 +17,25 @@ for _ in range(word_len):
 while not end_of_game:
     guess = input("Guess a letter: ").lower()
 
+    if guess in display:
+        print(f"You've already guessed {guess}")
+
     for position in range(word_len):
         letter = chosen_word[position]
+        # print(f"Current position: {position} \n Current letter: {letter} \n              Guessed letter {guess}")
         if letter == guess:
             display[position] = letter
     
     if guess not in chosen_word:
+        print(f"You guessed {guess}, thats not in the word. you lose a life")
         lives -= 1
         if lives == 0:
             end_of_game = True
             print("You Lose.")
+    print(f"{' '.join(display)}")
 
     if "_" not in display:
         end_of_game = True
         print("You Win!")
+
+    print(stages[lives])
