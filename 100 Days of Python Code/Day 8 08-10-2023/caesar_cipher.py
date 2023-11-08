7be770a14f7b1a23ab11ae9f26fbbdfd9b68a9d1
remijeous <remijeous@gmail.com>
2023-11-08 17:31:06 +0800

    Day 8 08-11-23 5.32pm

--- 100 Days of Python Code/Day 8 08-10-2023/caesar_cipher.py
+++ 100 Days of Python Code/Day 8 08-10-2023/caesar_cipher.py
@@ -1,24 +1,36 @@
+from art import logo
 alphabet = [
     'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
     'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
     'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
 ]
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
-
-def caesar_ciphar(start_text, shift_amount, direction):
+def caesar_ciphar(start_text, shift_amount, ciphar_direction):
     end_text = ""
-    if direction == "decode":
+    if ciphar_direction == "decode":
         shift_amount *= -1
-    for letter in start_text:
-        position = alphabet.index(letter)
-        new_position = position + shift_amount
-        end_text += alphabet[new_position]
-    print(f"Here's the {direction}d text: {end_text}")
+    for char in start_text:
+        if char in alphabet:
+            position = alphabet.index(char)
+            new_position = position + shift_amount
+            end_text += alphabet[new_position]
+        else:
+            end_text += char
+    print(f"Here's the {ciphar_direction}d text: {end_text}")
 
-caesar_ciphar(text, shift, direction)
+print(logo)
+
+should_end = False
+while not should_end:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    shift = shift % 26
+    caesar_ciphar(start_text=text, shift_amount=shift, ciphar_direction=direction)
+    restart = input("Type 'yes' if you want to go again. otherwise type = 'no'")
+    if restart == "no":
+        should_end = True
+        print("Goodbye.")
 # def encrypt(plain_text, shift_amount):
 #     cipher_text = ""
 #     for letter in plain_text:
